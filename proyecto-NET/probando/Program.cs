a7fbdf3b7b4631f9c3c8408e2ff8bfa5cf48bc02
Emil Colina <colinaleonemiljose@gmail.com>
2025-08-14 12:22:33 -0500

    eliminadas brechas de seguridad falsas

--- proyecto-NET/probando/Program.cs
+++ proyecto-NET/probando/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, PLatzi!");
-
-
-string Hola_mundo = "Server=localhost;Database;";
-string newLink = "Server=TU_SERVIDOR;Database=TU_BASE_DE_DATOS;User Id=TU_USUARIO;Password=TU_CONTRASEÑA;";
