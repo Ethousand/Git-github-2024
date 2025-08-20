e067526339051cd5dc388b117aa2417e401cf6bb
Emil Colina <colinaleonemiljose@gmail.com>
2025-08-20 12:58:50 -0500

    agregados paquetes obsoletos y dependabot

--- proyecto-NET/probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
+++ proyecto-NET/probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5264c6720dd7f0305afd31a0924c8db611809cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c684845857a08c9be106de2893f3babbae061368")]
 [assembly: System.Reflection.AssemblyProductAttribute("probando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
